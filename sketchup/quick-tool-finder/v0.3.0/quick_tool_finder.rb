42c22515273dd66d85e05840561f9a65e90cd83f
datphuho88-dev <datphuho88@gmail.com>
2026-09-17 10:26:25 +0700

    feat(sketchup): start Quick Tool Finder v0.3.0

---
+++ sketchup/quick-tool-finder/v0.3.0/quick_tool_finder.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module VADA
+  module QuickToolFinder
+    EXTENSION = SketchupExtension.new('Quick Tool Finder', 'quick_tool_finder/main') unless const_defined?(:EXTENSION, false)
+    EXTENSION.description = 'Fast command palette for SketchUp tools and extensions, with aliases, shortcuts, and pinned Quick Tools.'
+    EXTENSION.version = '0.3.0'
+    EXTENSION.creator = 'VADA / datphuho88-dev'
+    EXTENSION.copyright = '2026 VADA'
+
+    Sketchup.register_extension(EXTENSION, true)
+  end
+end
